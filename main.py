916707454bd5fb8420e05439989c4b32be4fa657
Raktabh <gitadb70@gmail.com>
2026-04-02 10:09:09 +0530

    intial boilerplate and databse setup next idea

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from fastapi import FastAPI
+﻿import uvicorn
 
-
-app = FastAPI()
-
-
-@app.get("/")
-def home():
-    return {"name": "Raktabh"}
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
