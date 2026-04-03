ee072e03be3c692477684a7bc60d3d722c7906e2
Raktabh <gitadb70@gmail.com>
2026-04-03 22:57:58 +0530

    added docuemnt model serach index and path endpoint

--- app/api/routes/documents.py
+++ app/api/routes/documents.py
@@ -1,1 +1,17 @@
-﻿
+﻿from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+from app.models.document import Document
+from app.schemas.document import DocumentCreate
+
+router = APIRouter(prefix="/documents")
+
+
+@router.post("/", status_code=201)
+def create_document(document_create: DocumentCreate, db: Session = Depends(get_db)):
+    document = Document(title=document_create.title, content=document_create.content)
+    db.add(document)
+    db.commit()
+    db.refresh(document)
+
+    return document
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 ﻿from app.db.base import Base
 from app.db.session import engine
 from app.models import document, search_index
+from app.api.routes import documents  
 from fastapi import FastAPI
 
+
 app = FastAPI()
+app.include_router(documents.router)
 Base.metadata.create_all(bind=engine)
